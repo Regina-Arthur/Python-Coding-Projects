c5a7c53e886e56048a1e9c821332e3d92def0429
ReginaEdithArthur <reginaeditharthur@gmail.com>
2025-09-24 14:03:56 +0000

    I added to the build of the system

--- Student_Grade_Management_System/Student_Grade_Management_System.py
+++ Student_Grade_Management_System/Student_Grade_Management_System.py
@@ -1,13 +1,28 @@
 #Greet users and ask for intent to use the system
 print ('Welcome, you can enter student grades for your course.')
-go_ahead = input ('Would you like to enter student grades? ')
+go_ahead = input ('Would you like to enter student grades?(yes/no) ')
 
 #confirm intent to use the system
-if go_ahead.upper() == "Yes":
+if go_ahead.upper() == "YES":
     start = 1
-else :
+elif go_ahead.upper() == "NO":
     start = 0
-#while start = 0:
+else:
+    print("Please enter a valid prompt.")
+
+while start == 1:
+    student_name = ()
+    
+    course = input("Please enter the course name: ")
+    number_students = int(input("How many students will you be entering?"))
+    for students in number_students:
+        print("""Please enter the student name, you will later be asked to enter their grade. 
+              This will be done one after the other until all student information has been entered.
+              Do not worry about errors, you will be allowed to correct that later.""")
+        student = input("Enter a student's name please: ")
+        student_name.student = int(input("Please enter their grade: "))
+
+
 
 
     
